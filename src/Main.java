7477bab1fbca3f3d93572615d51d9dbde5a418e0
minnesang <minnesangdev@gmail.com>
2024-11-15 17:22:45 +0200

    Added sum method

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, GitHub!");
+        System.out.println("Sum: " + sum(5, 10));
+    }
+
+    public static int sum(int a, int b) {
+        return a + b;
     }
 }
